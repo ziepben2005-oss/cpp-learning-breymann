a65f128afac16919cc13145ae0f57bbd0e2d6e62
Ben Ziep <ben.ziep@buhlergroup.com>
2026-06-17 18:56:39 +0200

    feat: Listing_1.2_Multiplikator add variable for multiplication

--- Teil_1_Einführung/Listing_1.2_Multiplikator.cpp
+++ Teil_1_Einführung/Listing_1.2_Multiplikator.cpp
@@ -5,13 +5,14 @@ int main()
 {
     int summand1 {0};
     int summand2 {0};
-    // Lies zwei Zahlen ein
-    cout << " Zwei ganze Zahlen eingeben: ";
-    cin >> summand1 >> summand2;
+    int summand3 {0};
+    // Lies drei Zahlen ein
+    cout << " Drei ganze Zahlen eingeben: ";
+    cin >> summand1 >> summand2 >> summand3;
     /*berechne das Produkt beider 
     Zahlen  */
-    int summe = summand1 * summand2;
+    int summe = summand1 * summand2 * summand3;
     // Zeige das Ergebnis auf dem Bildschirm an
-    cout << "Produkt=" << summe << '\n';
+    cout << "Nr.1 = " << summand1 << '\n' << "Nr.2 = " << summand2 << '\n' << "Nr.3 = " << summand3 << '\n' << "Produkt = " << summe << '\n';
     return 0;
 }
